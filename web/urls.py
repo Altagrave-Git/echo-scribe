f3eba239ea1cee7cb138bb0c9344d2b90506b855
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-04-18 20:49:34 -0600

    Delete web directory

--- web/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'web'
-
-urlpatterns = [
-    path('login', views.login, name='login'),
-    path('authorize', views.authorize, name='authorize'),
-]
