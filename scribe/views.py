153e13a4258f8415de7db2ad055529849e0c2ab2
altagrave <damon.j.turcotte@gmail.com>
2023-04-18 21:53:35 -0600

    Add basic profile view

--- scribe/views.py
+++ scribe/views.py
@@ -1,5 +1,10 @@
+from django.views.generic.base import TemplateView
 from django.shortcuts import render
 
 
 def index(request):
     return render(request, 'scribe/index.html')
+
+
+class ProfileView(TemplateView):
+    template_name = "profile.html"
