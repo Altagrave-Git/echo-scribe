3383838c31236872f09c7899738811a78ae9585c
altagrave <damon.j.turcotte@gmail.com>
2023-04-18 21:52:38 -0600

    Add echo network custom provider class

---
+++ customprovider/provider.py
@@ -0,0 +1,30 @@
+from allauth.socialaccount import providers
+from allauth.socialaccount.providers.base import ProviderAccount
+from allauth.socialaccount.providers.oauth2.provider import OAuth2Provider
+
+
+class CustomAccount(ProviderAccount):
+    pass
+
+
+class CustomProvider(OAuth2Provider):
+
+    id = 'customprovider'
+    name = 'Echo'
+    account_class = CustomAccount
+
+    def extract_uid(self, data):
+        return str(data['id'])
+
+    def extract_common_fields(self, data):
+        from pprint import pprint
+        return dict(uuid=data['uuid'],
+                    username=data['username'],
+                    avatar=data['avatar'])
+
+    def get_default_scope(self):
+        scope = ['read']
+        return scope
+
+
+providers.registry.register(CustomProvider)
